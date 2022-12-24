8ee961c145f978235b4593ecefbc4e21e275d54a
Abelitang_1 <121070479+gulangemilang@users.noreply.github.com>
2022-12-24 14:53:53 +0700

    tambahan abel

--- rental-car-system/app/controllers/CarController.java
+++ rental-car-system/app/controllers/CarController.java
@@ -6,14 +6,7 @@ import play.mvc.Result;
 
 public class CarController extends Controller {
     public Result addCar() {
-        Car car1 = new Car(1, "Toyota", "Corolla", 2018, 50.0);
-        car1.save();
-        Car car2 = new Car(2, "Honda", "Civic", 2017, 45.0);
-        car2.save();
-        Car car3 = new Car(3, "Ford", "Fiesta", 2016, 40.0);
-        car3.save();
-        Car car4 = new Car(4, "Mazda", "3", 2015, 35.0);
-        car4.save();
+       
         return ok("Car created successfully");
     }
 }
